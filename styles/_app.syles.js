38bfd5c520c033f48710d0680ac953addc146f56
Jose Alejandro <jose-a-mendez@hotmail.com>
2022-06-18 23:27:24 -0300

    add image background

--- styles/_app.syles.js
+++ styles/_app.syles.js
@@ -3,5 +3,9 @@ import { createGlobalStyle } from 'styled-components'
 export const GlobalStyle = createGlobalStyle`
   body {
     font-family: 'Mulish', sans-serif;
+    background-image: url('/images/bg.png');
+    background-attachment: fixed;
+    background-size: cover;
+    background-position: center;
   }
 `
