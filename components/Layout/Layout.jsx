86ff8b46c64f9035ed3b10007c69bec2a0d6339d
Jose Alejandro <jose-a-mendez@hotmail.com>
2022-06-18 23:14:15 -0300

    Create boilerplate

---
+++ components/Layout/Layout.jsx
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+
+const Layout = ({ children, title }) => {
+  return (
+    <>
+      <Head>
+        <title>{ title }</title>
+      </Head>
+
+      <main>{ children }</main>
+    </>
+  )
+}
+
+export default Layout
